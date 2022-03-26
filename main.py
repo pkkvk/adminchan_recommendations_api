8b16af10fb779a9d5ced4f539c4acb300cdc5ef3
Karthikeyan <karthikeyan.vijayan@openschool.tech>
2022-03-27 04:33:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ increment = (5-1)/total
 college_list ={}
 rating = 5.0
 for i,value in college_data.iterrows():
-    college_list[value["Name"]] = rating
+    college_list[rating] = value["Name"]
     rating = rating - increment
 
 # print(college_list)
@@ -36,8 +36,25 @@ for i,value in college_data.iterrows():
 #     else:
 #         return {}
 
-# def calculate_recommendations(GRE_score : int,TOEFL_score : int,\
-#     SOL_score : float,LOR_score : float,cgpa : float,research: bool):
+def calculate_recommendations(GRE_score : int,TOEFL_score : int,\
+    SOL_score : float,LOR_score : float,cgpa : float,research: bool):
+    y=[]
+    for rating,college in college_list.items():
+        y.append([GRE_score,TOEFL_score,rating,SOL_score,LOR_score,cgpa,research])
+    data=pd.DataFrame(y,columns=['GRE Score', 'TOEFL Score', 'University Rating', 'SOP', 'LOR ', 'CGPA', 'Research'])
+    pred = xgb_model_loaded.predict(data)
+    data["Chance of Admit"]=pred
+    count=0
+    recommendations = {}
+    for i,value in data.sort_values("Chance of Admit").iterrows():
+        recommendations[college_list[value["University Rating"]]]=value["Chance of Admit"]*100
+        count+=1
+        if count==10:
+            break
+    print(recommendations)
+
+
+calculate_recommendations(300,100,4.6,4.6,9.6,1)
 
 
 
